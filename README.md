65557fab5057c3bdc5c6a064dafe5a0dbc9f38f7
Gabriel Walter <gabrielwalter003@outlook.com.br>
2025-01-10 12:49:35 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Login-Auth-API
+
+API de autenticação e registro de usuários utilizando Spring Boot, com suporte a JWT e banco de dados H2.
+
+## Endpoints Principais
+- **POST /auth/register**: Registro de novos usuários.
+  - **Exemplo de JSON**:
+    ```json
+    {
+      "name": "John Doe",
+      "email": "john.doe@example.com",
+      "password": "senha123"
+    }
+    ```
+- **POST /auth/login**: Autenticação de usuários.
+  - **Exemplo de JSON**:
+    ```json
+    {
+      "email": "john.doe@example.com",
+      "password": "senha123"
+    }
+    ```
+- **GET /user**: Retorna informações do usuário autenticado (requer token JWT).
+  - **Header de Autorização**: `Authorization: Bearer <seu_token_aqui>`
+
+## Tecnologias Utilizadas
+- **Spring Boot**: Framework principal.
+- **Spring Security**: Gerenciamento de autenticação e autorização.
+- **JWT**: Tokens para autenticação.
+- **H2 Database**: Banco de dados em memória para testes.
+
+## Como Rodar Localmente
+1. Clone o repositório.
+2. Configure o ambiente com Java 17.
+3. Rode o comando: `./mvnw spring-boot:run`.
+4. Acesse a aplicação em `http://localhost:8080`.
+5. Para acessar o H2 Console: `http://localhost:8080/h2-console` (URL JDBC: `jdbc:h2:mem:testdb`).
+
+## Autor
+Projeto desenvolvido por Gabriel Walter Arantes.
+
+
+
---
+++ src/main/java/com/example/login_auth_api/domain/User.java
@@ -0,0 +1,63 @@
+package com.example.login_auth_api.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+
+    // Construtor sem argumentos
+    public User() {}
+
+    // Construtor com todos os argumentos
+    public User(String id, String name, String email, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters e Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
